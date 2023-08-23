9bc1694570cc444c5374eb50e60cc343950aa266
Mhiracle <73794838+Mhi-racle@users.noreply.github.com>
2023-08-23 18:01:30 +0000

    added beds

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ import Homepage from "@public/assets/images/homepage.jpg";
 import grid from "@public/assets/images/9.jpg"
 import wardrobeimg from "public/assets/images/bilal-mansuri-RHARVor_ydc-unsplash.jpg"
 import chair from "public/assets/images/steve-johnson-MqP5vi4IuSU-unsplash.jpg"
+import bed from "public/assets/images/deconovo-gVKmonDbotU-unsplash.jpg"
 import table from "public/assets/images/side-view-cup-coffee-small-round-table-horizontal.jpg"
 import phone from "public/assets/images/PngItem_202171.png"
 import email from "public/assets/images/NicePng_email-icon-white-png_2226601.png"
@@ -26,9 +27,10 @@ const Home = () => {
       
     />
     </section>
+    
+    {/* Created to create thr illusion on gradient */}
     <section className=' w-screen h-[460px] absolute top-6 bg-gradient-to-t from-neutral-100 to-transparent md:bg-gradient-to-r md:from-neutral-100 md:to-transparent md:h-[911px]'>
-    
-    
+  
     </section>
 
       {/*header of our customisation website*/}
@@ -41,6 +43,7 @@ const Home = () => {
     <PromptCard/>
     </div>
 
+{/* Table Section */}
     <section className=' flex app w-full md:flex-row gap-20 '>
     <section className=' hover:scale-95 duration-300 bg-amber-300 mt-28 w-[580px] h-[300px]'>
     <Image className='ml-[22px] -mt-5 w-[580px] h-[300px] hover:relative duration-300 hover:scale-95'
@@ -50,10 +53,8 @@ const Home = () => {
     </section>
     
 
-
     <section className=' mr-48 ml-12 w-96'>
     <p className='font-bold text-[33px]  mt-[70px]'>Tables</p>
-    
     <p className='mt-[50px] text-[15px] font-serif'>Discover timeless elegance with our stylish table collection.  
     From sleek and modern design, our tables are the perfect addition to any space. 
     Create the table of your dreams with our furniture customization platform, that brings your vision to life and watch your space transform with a table that's truly one-of-a-kind. 
@@ -68,14 +69,13 @@ const Home = () => {
       </ul>
     </div>
 
-    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[40px] text-[15px]'>
+    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[60px] text-[15px] hover:scale-105 duration-300'>
       Open Configurator
       </button>
     </section>
 
+  {/* Chairs */}
     </section>
-
-
     <section className=' flex app w-full md:flex-row-reverse'>
     <section className=' hover:scale-95 duration-300 bg-amber-300 mt-28 w-[580px] h-[300px]'>
     <Image className='ml-[22px] -mt-5 w-[580px] h-[300px] hover:relative duration-300 hover:scale-95'
@@ -85,10 +85,11 @@ const Home = () => {
     </section>
 
 
+  
     <section className=' mr-48 ml-12 w-96 '>
     <p className='font-bold text-[33px]  mt-[70px]'>Chairs</p>
     
-    <p className='mt-[50px] text-[15px] font-serif'> Our stylish table models offers you the option to modify one of our available designs by givin you the option to:
+    <p className='mt-[50px] text-[15px] font-serif'> Our stylish chair models offers you the option to modify one of our available designs by giving you the option to:
     </p>
 
     <div className='mt-[20px] text-[15px] font-serif'>
@@ -100,13 +101,14 @@ const Home = () => {
       </ul>
     </div>
 
-    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[40px] text-[15px]'>
+    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[60px] text-[15px] hover:scale-105 duration-300'>
       Open Configurator
       </button>
     </section>
 
     </section>
 
+  {/* Wardrobes */}
     <section className=' flex app w-full md:flex-row gap-16'>
     <section className=' hover:scale-95 duration-300 bg-amber-300 mt-28 w-[580px] h-[300px]'>
     <Image className='ml-[22px] -mt-5 w-[580px] h-[300px] hover:relative duration-300 hover:scale-95'
@@ -115,7 +117,7 @@ const Home = () => {
     /> 
     </section>
 
-
+  
     <section className=' mr-48 ml-12 w-96'>
     <p className='font-bold text-[33px]  mt-[70px]'>Wardrobes</p>
     
@@ -131,27 +133,32 @@ const Home = () => {
       </ul>
     </div>
 
-    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[40px] text-[15px]'>
+    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[60px] text-[15px] hover:scale-105 duration-300'>
       Open Configurator
       </button>
     </section>
 
     </section>
 
-
+ {/* Beds */}
     <section className=' flex app w-full md:flex-row-reverse'>
     <section className=' hover:scale-95 duration-300 bg-amber-300 mt-28 w-[580px] h-[300px]'>
     <Image className='ml-[22px] -mt-5 w-[580px] h-[300px] hover:relative duration-300 hover:scale-95'
-        src={grid}
+        src={bed}
         alt='grid'
     /> 
     </section>
 
 
+   
     <section className=' mr-48 ml-12 w-96 '>
     <p className='font-bold text-[33px]  mt-[70px]'>Chairs</p>
     
-    <p className='mt-[50px] text-[15px] font-serif'> Our stylish table models offers you the option to modify one of our available designs by givin you the option to:
+    <p className='mt-[50px] text-[15px] font-serif'> 
+   
+Embrace the transformative power of a well-chosen bed as it becomes the centerpiece of your bedroom. Elevate your space with the harmonious blend of design, comfort, and craftsmanship that our beds bring. 
+  Explore our collection today and embark on a journey to create a haven of relaxation and style in your home.
+   This is possible by modifying one of our available designs by giving you the opportunity to change:
     </p>
 
     <div className='mt-[20px] text-[15px] font-serif'>
@@ -163,7 +170,7 @@ const Home = () => {
       </ul>
     </div>
 
-    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[40px] text-[15px]'>
+    <button className=' font-serif bg-black text-white mt-[50px] w-[250px] h-[60px] text-[15px] hover:relative duration-300 hover:scale-95'>
       Open Configurator
       </button>
     </section>
@@ -171,7 +178,7 @@ const Home = () => {
     </section>
 
 
-    <section className=' text-white p-10 bg-black w-full h-[250px] mt-[50px]'>
+    <footer className=' text-white p-10 grid m-0 bg-black w-screen h-[250px] mt-[50px]'>
       <p className='  ml-10 '>Contact us</p>
       <section className='   md:flex-row'>
       <div className='flex gap-4 mt-8 ml-10'>
@@ -198,17 +205,9 @@ const Home = () => {
      
       
 
-    </section>
+    </footer>
 
 
-
-
-
-    
-
-
-
-  
     </section>
     
    
